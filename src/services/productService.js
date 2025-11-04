66fb49d2bebac0c2db5c1e43c7399e18c6a921bd
Soumika Devarakonda <142423331+soumikadevarakonda@users.noreply.github.com>
2025-11-04 15:06:19 +0530

    Update API base URL to localhost
    Changed API base URL to localhost for local development.

--- src/services/productService.js
+++ src/services/productService.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_BASE_URL = "http://ec2-16-170-7-5.eu-north-1.compute.amazonaws.com:30082/back1/api/products";  // Updated Backend API URL
+const API_BASE_URL = "http://localhost:30082/back1/api/products";  // Updated Backend API URL
 
 // Fetch products by category (computers, mobiles, laptops, pendrives, or all)
 export const getProducts = async (category = "") => {
